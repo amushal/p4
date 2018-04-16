7d40e26aab527eaafce6e2833469ad9f1b99ceb4
Ala Mushal <amushal@hotmail.com>
2018-04-16 12:19:59 -0700

    Added deploy route to test db

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,45 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::user()){
+        return view('home');
+    }else{
+        return view('welcome');
+    }
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+        'Database defaultStringLength' => Illuminate\Database\Schema\Builder::$defaultStringLength,
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
